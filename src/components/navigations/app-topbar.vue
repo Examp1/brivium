46ca3c06e092df72b019af3e0952b3469546b329
Examp1 <examp1.web@gmail.com>
2025-08-26 18:27:12 +0300

    add app-top-bar component

--- src/components/navigations/app-topbar.vue
+++ src/components/navigations/app-topbar.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import loadNotifications from "@/composables/notifications/load-notifications";
-loadNotifications().then((res) => {
-    notifications.value = res.data.value;
-});
+import { onMounted } from "vue";
 
 const notificationStore = useNotificationStore();
 const { notifications } = storeToRefs(notificationStore);
 
 const companyInfoStore = useCompanyInfoStore();
 const { companyInfo } = storeToRefs(companyInfoStore);
+
+const isDropOpen = ref(false);
+
+onMounted(async () => {
+    const { data } = await loadNotifications();
+    notifications.value = data.value;
+});
 </script>
 
 <template>
@@ -22,11 +27,22 @@ const { companyInfo } = storeToRefs(companyInfoStore);
         </button>
 
         <div class="flex items-center gap-3">
-            <div class="relative mdi mdi-bell text-3xl">
-                <span
-                    class="absolute text-xs bg-blue-500 w-fit min-w-4 h-4 flex items-center justify-center rounded-full top-0 right-0 text-white"
-                    >{{ notifications.items.length }}</span
+            <div class="notifications-wrapper relative">
+                <div
+                    class="relative mdi mdi-bell text-3xl cursor-pointer"
+                    @click="isDropOpen = !isDropOpen"
                 >
+                    <span
+                        class="absolute text-xs bg-blue-500 w-fit min-w-4 h-4 flex items-center justify-center rounded-full top-0 right-0 text-white"
+                        >{{ notifications?.items?.length }}</span
+                    >
+                </div>
+                <div
+                    class="notifications absolute w-[300px] left-[50%] translate-x-[-50%] bg-white shadow-2xl p-4 rounded-lg transition-all duration-200 ease-out scale-95 opacity-0"
+                    :class="{ 'scale-100 opacity-100': isDropOpen }"
+                >
+                    {{ notifications }}
+                </div>
             </div>
             <img
                 class="h-[44px] w-[44px] rounded-full"
